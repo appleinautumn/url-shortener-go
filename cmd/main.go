78a86d76e135b59a3d24624c61984a662806d759
Firdaus Adi Putra <mail@firdaus.org>
2025-05-28 18:00:33 +0700

    Move routes

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"database/sql"
-	"encoding/json"
-	"fmt"
 	"log"
-	"math/rand/v2"
 	"net/http"
 	"os"
 
+	"url-shortener-go/internal/routes"
 	"url-shortener-go/storage"
 
-	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 )
 
@@ -36,14 +32,7 @@ func main() {
 	}
 	defer storage.CloseDB()
 
-	r := chi.NewRouter()
-
-	r.Post("/shorten", createShortURL)
-	r.Get("/{shortID}", getURL)
-
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("URL Shortener"))
-	})
+	r := routes.Routes()
 
 	appPort := os.Getenv("APP_PORT")
 	if appPort == "" {
@@ -52,40 +41,4 @@ func main() {
 
 	log.Printf("Listening on :%s...", appPort)
 	http.ListenAndServe(":"+appPort, r)
-}
-
-func createShortURL(w http.ResponseWriter, r *http.Request) {
-	var input URLMapping
-	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-		http.Error(w, "Invalid JSON", http.StatusBadRequest)
-		return
-	}
-
-	// Generate random short ID
-	short := fmt.Sprintf("%x", rand.IntN(100000))
-
-	if err := storage.StoreURL(short, input.Long); err != nil {
-		http.Error(w, "Failed to store URL", http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(URLMapping{Short: short, Long: input.Long})
-}
-
-func getURL(w http.ResponseWriter, r *http.Request) {
-	shortID := chi.URLParam(r, "shortID")
-
-	var longURL string
-	longURL, err := storage.GetLongURL(shortID)
-	if err == sql.ErrNoRows {
-		http.NotFound(w, r)
-		return
-	} else if err != nil {
-		http.Error(w, "Failed to retrieve URL", http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(URLMapping{Short: shortID, Long: longURL})
 }
