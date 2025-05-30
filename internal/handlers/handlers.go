fd043e5a408d54923889e39cbb7d018078eeefae
Firdaus Adi Putra <mail@firdaus.org>
2025-05-30 11:50:27 +0700

    Add handlers

---
+++ internal/handlers/handlers.go
@@ -0,0 +1,54 @@
+package handlers
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+
+	"url-shortener-go/storage"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type URLMapping struct {
+	Short string `json:"short"`
+	Long  string `json:"long"`
+}
+
+func CreateShortURL(w http.ResponseWriter, r *http.Request) {
+	var input URLMapping
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	// Generate random short ID
+	short := fmt.Sprintf("%x", rand.Intn(100000))
+
+	if err := storage.StoreURL(short, input.Long); err != nil {
+		http.Error(w, "Failed to store URL", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(URLMapping{Short: short, Long: input.Long})
+}
+
+func GetURL(w http.ResponseWriter, r *http.Request) {
+	shortID := chi.URLParam(r, "shortID")
+
+	var longURL string
+	longURL, err := storage.GetLongURL(shortID)
+	if err == sql.ErrNoRows {
+		http.NotFound(w, r)
+		return
+	} else if err != nil {
+		http.Error(w, "Failed to retrieve URL", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(URLMapping{Short: shortID, Long: longURL})
+}
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,13 +1,9 @@
 package routes
 
 import (
-	"database/sql"
-	"encoding/json"
-	"fmt"
-	"math/rand/v2"
 	"net/http"
 
-	"url-shortener-go/storage"
+	"url-shortener-go/internal/handlers"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -20,48 +16,12 @@ type URLMapping struct {
 func Routes() *chi.Mux {
 	r := chi.NewRouter()
 
-	r.Post("/shorten", createShortURL)
-	r.Get("/{shortID}", getURL)
+	r.Post("/shorten", handlers.CreateShortURL)
+	r.Get("/{shortID}", handlers.GetURL)
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("URL Shortener"))
 	})
 
 	return r
-}
-
-func createShortURL(w http.ResponseWriter, r *http.Request) {
-	var input URLMapping
-	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-		http.Error(w, "Invalid JSON", http.StatusBadRequest)
-		return
-	}
-
-	// Generate random short ID
-	short := fmt.Sprintf("%x", rand.IntN(100000))
-
-	if err := storage.StoreURL(short, input.Long); err != nil {
-		http.Error(w, "Failed to store URL", http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(URLMapping{Short: short, Long: input.Long})
-}
-
-func getURL(w http.ResponseWriter, r *http.Request) {
-	shortID := chi.URLParam(r, "shortID")
-
-	var longURL string
-	longURL, err := storage.GetLongURL(shortID)
-	if err == sql.ErrNoRows {
-		http.NotFound(w, r)
-		return
-	} else if err != nil {
-		http.Error(w, "Failed to retrieve URL", http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(URLMapping{Short: shortID, Long: longURL})
 }
