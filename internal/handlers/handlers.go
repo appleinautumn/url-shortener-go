c09928b86a42501d2f93e423dee5e252d79b3f04
Firdaus Adi Putra <mail@firdaus.org>
2025-06-03 14:42:37 +0700

    Log in handlers

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log/slog"
 	"math/rand"
 	"net/http"
 
@@ -20,6 +21,7 @@ type URLMapping struct {
 func CreateURL(w http.ResponseWriter, r *http.Request) {
 	var input URLMapping
 	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		slog.Error("Invalid JSON in request body", "error", err)
 		http.Error(w, "Invalid JSON", http.StatusBadRequest)
 		return
 	}
@@ -28,10 +30,12 @@ func CreateURL(w http.ResponseWriter, r *http.Request) {
 	short := fmt.Sprintf("%x", rand.Intn(100000))
 
 	if err := storage.StoreURL(short, input.Long); err != nil {
+		slog.Error("Failed to store URL", "error", err, "short", short, "long", input.Long)
 		http.Error(w, "Failed to store URL", http.StatusInternalServerError)
 		return
 	}
 
+	slog.Info("URL created", "short", short, "long", input.Long)
 	json.NewEncoder(w).Encode(URLMapping{Short: short, Long: input.Long})
 }
 
@@ -41,12 +45,15 @@ func GetURL(w http.ResponseWriter, r *http.Request) {
 	var longURL string
 	longURL, err := storage.GetLongURL(shortID)
 	if err == sql.ErrNoRows {
+		slog.Warn("URL not found", "short", shortID)
 		http.NotFound(w, r)
 		return
 	} else if err != nil {
+		slog.Error("Failed to retrieve URL", "error", err, "short", shortID)
 		http.Error(w, "Failed to retrieve URL", http.StatusInternalServerError)
 		return
 	}
 
+	slog.Info("URL retrieved", "short", shortID, "long", longURL)
 	json.NewEncoder(w).Encode(URLMapping{Short: shortID, Long: longURL})
 }
